344901bcfc0606e687b906e8dbdc64ecef6a4ffb
Demonk <rawalkundan987@gmail.com>
2026-04-06 01:08:50 +0530

    Resolved bugs

--- attendance_backend/database.py
+++ attendance_backend/database.py
@@ -10,9 +10,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL not set in .env")
-
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
+elif DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
+    
 engine = create_engine(
     DATABASE_URL.replace("postgresql://", "postgresql+psycopg://"),
     pool_pre_ping=True
